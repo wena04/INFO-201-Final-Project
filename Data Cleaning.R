42bc5e4cf7cde7870cbe1dfd94b8d80325b5aa13
ici Su <syx0722@uw.edu>
2024-02-20 14:27:12 -0800

    update some code

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -60,5 +60,24 @@ final_df <- rename(final_df,"Country"="Country.name","Climate_Characteristic"="S
 final_df <- final_df %>% select(Country,Year,everything())
 
 final_df$Climate_Characteristic
-#removing rows with characteristics that we are not looking for
-final_df <- final_df %>% filter(!Climate_Characteristic %in% c("Land area below 5m (% of land area)","Ceral__________"))
+#removing rows with climax_characteristics that we are not looking for
+final_df <- final_df %>% filter(!Climate_Characteristic %in% c("Land area below 5m (% of land area)","Agricultural land under irrigation (% of total ag. land)","Nitrous oxide (N2O) emissions, total (KtCO2e)","Other GHG emissions, total (KtCO2e)","Disaster risk reduction progress score (1-5 scale; 5=best)","GHG net emissions/removals by LUCF (MtCO2e)", "Hosted Joint Implementation (JI) projects","Access to improved water source (% of total pop.)", "Nurses and midwives (per 1,000 people)","Access to improved sanitation (% of total pop.)","Child malnutrition, underweight (% of under age 5)", "Population living below $1.25 a day (% of total)","Population growth (annual %)","Cereal yield (kg per hectare)","Physicians (per 1,000 people)", "Malaria incidence rate (per 100,000 people)", "Urban population growth (annual %)", "Urban population","Methane (CH4) emissions, total (KtCO2e)", "Annex-I emissions reduction target","Hosted Clean Development Mechanism (CDM) projects","Issued Certified Emission Reductions (CERs) from CDM (thousands)
+","Droughts, floods, extreme temps (% pop. avg. 1990-2009)", "NAPA submission","Latest UNFCCC national communication","Renewable energy target", "Population in urban agglomerations >1million (%)","Ease of doing business (ranking 1-183; 1=best)","Invest. in energy w/ private participation ($)","Ratio of girls to boys in primary & secondary school (%)","Primary completion rate, total (% of relevant age group)","Under-five mortality rate (per 1,000)","Population
+","GNI per capita (Atlas $)","Paved roads (% of total roads)","Public sector mgmt & institutions avg. (1-6 scale; 6=best)","Invest. in water/sanit. w/ private participation ($)","Annual freshwater withdrawals (% of internal resources)","Population below 5m (% of total)","Invest. in telecoms w/ private participation ($)","Projected change in annual hot days/warm nights","Access to electricity (% of total population)","Foreign direct investment, net inflows (% of GDP)","NAMA submission","Nationally terrestrial protected areas (% of total land area)","Invest. in transport w/ private participation ($)","GNI per capita (Atlas $)","Population","Energy use per units of GDP (kg oil eq./$1,000 of 2005 PPP $)","Energy use per capita (kilograms of oil equivalent)","CO2 emissions, total (KtCO2)","Average annual precipitation (1961-1990, mm)","Issued Certified Emission Reductions (CERs) from CDM (thousands)","Issued Certified Emission Reductions (CERs) from CDM (thousands)","Average daily min/max temperature (1961-1990, Celsius)","Projected annual temperature change (2045-2065, Celsius)","Projected change in annual cool days/cold nights","CO2 emissions per units of GDP (kg/$1,000 of 2005 PPP $)","Issued Emission Reduction Units (ERUs) from JI (thousands)","Projected annual precipitation change (2045-2065, mm)","GDP"))
+
+#removing rows with Economy_characteristics that we are not looking for
+final_df <- final_df %>% filter(!Economy_Characteristic %in% c("Gross domestic product corresponding to fiscal year, current prices","Total investment","Gross domestic product, current prices","Gross domestic product per capita, current prices"))
+
+#removing rows with Countries lack of  useful information 
+final_df <- final_df %>% filter(!Country %in% c("Afghanistan","Belarus","Azerbaijan"))
+
+#removing rows with years with 1 year time gap 
+final_df <- final_df %>% filter(!Year %in% c("1991","1993","1995","1997","1999","2001", "2003", "2005","2007","2009","2011"))
+
+
+#test_df<-final_df %>% 
+#  filter(Economy_Values == "n/a" & Economy_Characteristic=="Population")
+  
+nrow(final_df)
+
+
