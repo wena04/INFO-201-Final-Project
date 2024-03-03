a91cf8fe8cc9b1736cf97e1051029abc5b1ec432
Anthony-Wen <anthonywen04@gamil.com>
2024-03-02 17:47:04 -0800

    edited some formmating

--- Shiny/server.R
+++ Shiny/server.R
@@ -7,50 +7,36 @@ library(bslib)
 library(shiny)
 #loading the final csv file from the data wrangling assignment
 final_df <- read.csv("www/final_df.csv")
+
+#Providing all the characteristics choices for the second tab while mapping each map to its corresponding column in the final_df column 
 characteristic_mapping <- c("GDP" = "Gdp","GDP per Capita" = "Gdp_per_cap","Average Temperature" = "Average_temp","CO2 Emissions" = "CO2_emissions","Population" = "Population","Cereal Yield" = "Cereal_yield")
 
 server <- function(input, output, session) {
   
-  #Plotting the Country Heat Maps for Average Temperature
+  #Plotting the Country Heat Maps for Average Temperature (Tab 1)
   output$World_map <- renderPlotly({
+    #Getting the input from the slider
     selected_year = input$Heat_slider
-    #loading the world map
+    #loading the world map data
     world_map <- map_data("world")
     #creating a dataframe with only the selected year
     sel_year_df <- final_df %>% filter(Year == selected_year)
     #joining the longitutde and latitude datas on the world with the final_df
     world_with_temp <-
       world_map %>% left_join(sel_year_df, by = c("region" = "Country"))
-    #plotting the graph
-    wm <-
-      ggplot(data = world_with_temp,
-             aes(
-               x = long,
-               y = lat,
-               group = group,
-               fill = Average_temp,
-               text = paste0(
-                 "Country: ",
-                 region,
-                 "<br>",
-                 "Average Temperature: ",
-                 round(Average_temp, 5)
-               )
-             )) + scale_fill_continuous(
-               low = "#F3E0A5",
-               high = "#ED5A26",
-               limits = c(-10, 30)
-             ) + geom_polygon(aes(fill = Average_temp)) + labs(
-               title = paste("Average Temperature By Country in",
-                             selected_year),
-               fill = "Temperature (C)"
-             ) + theme_gray()
+    #plotting the graph, filling each country shape with the average temperature
+    wm <- ggplot(data = world_with_temp,aes(x = long, y = lat, group = group, fill = Average_temp, text = paste0("Country: ", region, "<br>", "Average Temperature: ", round(Average_temp, 5)))) 
+    #changing the color of the legend/scale on the right side along with its limits
+    + scale_fill_continuous(low = "#F3E0A5", high = "#ED5A26", limits = c(-10, 30)) + geom_polygon(aes(fill = Average_temp)) 
+    #changing the title of the graph and the theme 
+    + labs(title = paste("Average Temperature By Country in", selected_year), fill = "Temperature (C)") + theme_gray()
     #render the plotly back to ui page
     return(ggplotly(wm, tooltip = "text"))
   })
   
-  #Plotting the Country Heat Maps for Carbon Emission
+  #Plotting the Country Heat Maps for Carbon Emissions (Tab 1)
   output$CO2_map <- renderPlotly({
+    #Getting the input from the slider
     selected_year = input$Heat_slider
     #loading the world map
     world_map <- map_data("world")
@@ -59,30 +45,12 @@ server <- function(input, output, session) {
     #joining the longitutde and latitude datas on the world with the final_df
     world_with_co2 <-
       world_map %>% left_join(sel_year_df, by = c("region" = "Country"))
-    #plotting the graph
-    wmc <-
-      ggplot(data = world_with_co2,
-             aes(
-               x = long,
-               y = lat,
-               group = group,
-               fill = CO2_emissions,
-               text = paste0(
-                 "Country: ",
-                 region,
-                 "<br>",
-                 "Carbon Emissions Rate: ",
-                 CO2_emissions
-               )
-             )) + scale_fill_continuous(
-               low = "#F3E0A5",
-               high = "#ED5A26",
-               limits = c(0, 20)
-             ) + geom_polygon(aes(fill = CO2_emissions)) + labs(
-               title = paste("Carbon Emissions By Country in",
-                             selected_year),
-               fill = "CO2 (metric tons)"
-             ) + theme_gray()
+    #plotting the graph, filling each country shape with the carbon emissions
+    wmc <- ggplot(data = world_with_co2, aes(x = long, y = lat, group = group, fill = CO2_emissions, text = paste0("Country: ", region, "<br>", "Carbon Emissions Rate: ", CO2_emissions))) 
+    #changing the color of the legend/scale on the right side along with its limits
+    + scale_fill_continuous(low = "#F3E0A5", high = "#ED5A26", limits = c(0, 20)) + geom_polygon(aes(fill = CO2_emissions)) 
+    #changing the title of the graph and the theme 
+    + labs(title = paste("Carbon Emissions By Country in", selected_year), fill = "CO2 (metric tons)") + theme_gray()
     #render the plotly back to ui page
     return(ggplotly(wmc, tooltip = "text"))
   })
